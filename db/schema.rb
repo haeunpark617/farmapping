cf07f8c199ada85b548240b734773e77a99ed156
parkhaeun <syhe617@gmail.com>
2016-08-20 14:31:34 +0900

    convert infowidnow to overlay & add picutre

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810063815) do
+ActiveRecord::Schema.define(version: 20160820050201) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160810063815) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "view"
+    t.string   "picture"
   end
 
 end
