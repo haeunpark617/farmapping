cad27ae534b886efd9847fcdac6f6b7d868ddbd6
parkhaeun <syhe617@gmail.com>
2016-08-20 15:01:12 +0900

    adjust pictureuploader

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,17 +2,6 @@
 
 class PictureUploader < CarrierWave::Uploader::Base
 
-  after :remove, :delete_empty_upstream_dirs
-
-  def delete_empty_upstream_dirs
-    path = ::File.expand_path(store_dir, root)
-    Dir.delete(path) # fails if path not empty dir
-
-  path = ::File.expand_path(base_store_dir, root)
-    Dir.delete(path) # fails if path not empty dir
-    rescue SystemCallError
-    true # nothing, the dir is not empty
-    end
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
